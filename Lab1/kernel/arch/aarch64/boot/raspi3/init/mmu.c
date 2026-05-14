468dcf358c5bb57070e90b680232b8b27b6154dc
ShaneGu2002 <gushengfei2002@gmail.com>
2026-05-14 08:37:20 +0800

    finish lab1

--- Lab1/kernel/arch/aarch64/boot/raspi3/init/mmu.c
+++ Lab1/kernel/arch/aarch64/boot/raspi3/init/mmu.c
@@ -20,6 +20,8 @@ typedef unsigned int u32;
 #define PERIPHERAL_BASE (0x3F000000UL)
 #define PHYSMEM_END     (0x40000000UL)
 
+#define KERNAL_VADDR    0xffffff0000000000
+
 /* The number of entries in one page table page */
 #define PTP_ENTRIES 512
 /* The size of one page table page */
@@ -88,14 +90,39 @@ void init_kernel_pt(void)
         /* LAB 1 TODO 5 BEGIN */
         /* Step 1: set L0 and L1 page table entry */
         /* BLANK BEGIN */
+        vaddr = KERNAL_VADDR + PHYSMEM_START;
+
+        boot_ttbr1_l0[GET_L0_INDEX(vaddr)] = ((u64)boot_ttbr1_l1) | IS_TABLE
+                                             | IS_VALID;
+        boot_ttbr1_l1[GET_L1_INDEX(vaddr)] = ((u64)boot_ttbr1_l2) | IS_TABLE
+                                             | IS_VALID;
         /* BLANK END */
 
         /* Step 2: map PHYSMEM_START ~ PERIPHERAL_BASE with 2MB granularity */
         /* BLANK BEGIN */
+        for (; vaddr < KERNAL_VADDR + PERIPHERAL_BASE; vaddr += SIZE_2M) {
+                boot_ttbr1_l2[GET_L2_INDEX(vaddr)] =
+                        (vaddr - KERNAL_VADDR) /* low mem, va = pa */
+                        | UXN /* Unprivileged execute never */
+                        | ACCESSED /* Set access flag */
+                        | NG /* Mark as not global */
+                        | INNER_SHARABLE /* Shareability */
+                        | NORMAL_MEMORY /* Normal memory */
+                        | IS_VALID;
+        }
         /* BLANK END */
 
         /* Step 2: map PERIPHERAL_BASE ~ PHYSMEM_END with 2MB granularity */
         /* BLANK BEGIN */
+        for (vaddr = KERNAL_VADDR + PERIPHERAL_BASE; vaddr < KERNAL_VADDR + PHYSMEM_END; vaddr += SIZE_2M) {
+                boot_ttbr1_l2[GET_L2_INDEX(vaddr)] =
+                        (vaddr - KERNAL_VADDR) /* low mem, va = pa */
+                        | UXN /* Unprivileged execute never */
+                        | ACCESSED /* Set access flag */
+                        | NG /* Mark as not global */
+                        | DEVICE_MEMORY /* Device memory */
+                        | IS_VALID;
+        }
         /* BLANK END */
         /* LAB 1 TODO 5 END */
 
